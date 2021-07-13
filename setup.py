8f90b90192c9396d1b2c53cf8164c41bbf0718ce
betsy <elizabeth.alpert@qut.edu.au>
2021-07-13 15:42:48 +1000

    Add setup.py for local installation

---
+++ setup.py
@@ -0,0 +1,39 @@
+# Bits copied from https://github.com/pypa/sampleproject/blob/main/setup.py
+from setuptools import setup, find_packages
+import pathlib
+
+
+install_requires = [
+    'click>=8.0.1'
+]
+
+
+here = pathlib.Path(__file__).parent.resolve()
+
+# Get the long description from the README file
+long_description = (here / 'readme.md').read_text(encoding='utf-8')
+
+setup(
+    name='gab_tidy_data',
+    description='Python script to take gab data (from Garc) and put it into a '
+                'relational SQLite database ',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    author='QUT Digital Observatory',
+    author_email='digitalobservatory@qut.edu.au',
+    url='https://github.com/QUT-Digital-Observatory/gab_tidy_data',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Programming Language :: Python :: 3'
+    ],
+    use_scm_version=True,
+    setup_requires=['setuptools_scm'],
+    packages=find_packages(),
+    python_requires='>=3.8',
+    install_requires=install_requires,
+    entry_points={
+        "console_scripts": [
+            "gab_tidy_data = gab_tidy_data.__main__:tidy_data"
+        ]
+    }
+)
